9adc3821cafbb5bf99b0a263078ed6a02776170e
wanglei <2365104338@qq.com>
2020-09-09 16:20:31 +0800

    增加时间线

--- src/pages/alertjob/detail/alertJobDetail.tsx
+++ src/pages/alertjob/detail/alertJobDetail.tsx
@@ -1,6 +1,10 @@
 import React, {Component} from "react";
 import {Image, Text, View} from "@tarojs/components";
-import './alertJobDetail.css'
+import './alertJobDetail.css';
+import {AtTimeline} from 'taro-ui'
+
+import "taro-ui/dist/style/components/timeline.scss";
+import "taro-ui/dist/style/components/icon.scss";
 
 export default class AlertJobDetail extends Component {
 
@@ -19,8 +23,19 @@ export default class AlertJobDetail extends Component {
   componentDidHide() {
   }
 
+  onChange() {
+    // this.setState({
+    //   current
+    // })
+  }
 
   render() {
+    const items = [
+      {'title': '步骤一', 'desc': '这里是额外的信息，最多两行'},
+      {'title': '步骤二', 'desc': '这里是额外的信息，最多两行'},
+      {'title': '步骤三', 'desc': '这里是额外的信息，最多两行'}
+    ]
+
     return (
       <View id='box'>
         <Image className='img_main' src={'http://192.168.2.8/boss/uploads4/images/4635/46357740/v2_q7y2lx.png'}/>
@@ -34,6 +49,20 @@ export default class AlertJobDetail extends Component {
           <View className='title_pre'/>
           <Text className='title_1'>警情状态</Text>
         </View>
+
+        <AtTimeline
+          className='at_time_line'
+          items={[
+            {title: "智能报警", content: ['王蒙、秦邵华'], icon: 'check-circle', color: 'blue'},
+            {title: '通知出警', content: ['秦邵华'],icon: 'check-circle', color: 'blue'},
+            {title: '已接警', color: 'red'},
+            {title: '到达现场', color: 'red'},
+            {title: '出警结束', color: 'red'},
+            {title: '结案确认', color: 'red'},
+          ]}
+          pending={true}
+        >
+        </AtTimeline>
       </View>
     )
   }
